a4219059d54ed2caa55a32e9de04291ca00cde8d
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-18 00:18:01 +0100

    Update and rename asm 03-09-2020.asm to exercise1.asm

--- exercise1.asm
+++ exercise1.asm
@@ -1,22 +1,3 @@
-;# int somma_pari(char *st, int d)
-;# { int i,somma;
-;# somma=0;
-;# for(i=0;i<d;i++)
-;# if(st[i]%2==0)
-;# somma++;
-;# return somma;
-;# }
-;# main() {
-;# char ST[16];
-;# int i,cnt;
-;# for(i=0;i<2;i++){
-;# printf("Inserisci una stringa di numeri)\n");
-;# scanf("%s",ST);
-;# cnt=somma_pari(ST,strlen(ST));
-;# printf(" Numero= %d \n",cnt);
-;# }
-;# }
-
 .data 
 ST: .space 16 ;# ST[16]
 stack: .space 32
